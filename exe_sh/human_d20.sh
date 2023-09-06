68f77ee246802bc85ce31cae6ba487e71ac4318d
jungle401 <johnson401.cs09@nycu.edu.tw>
2023-09-06 22:48:38 +0800

    init commit

---
+++ exe_sh/human_d20.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# rm -rf /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/tmp1/
+# /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/self_raw_dots/fw/\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/self_raw_dots
+#
+# /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/rp_raw_dots/\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/rp_raw_dots
+#
+# /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/self_non_micro_segs/fw/\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/self_non_micro_segs
+#
+# /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/self_non_micro_cluss/fw\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/self_non_micro_cluss
+#
+/mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+  /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/non_micro_segs/fw/\
+  /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/non_micro_segs
+
+# /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/build/app\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/stable_ver/output/pbsim/human/T2T/chr22/d20/extended/fw/\
+#   /mnt/es/ness/johnson/thesis/overlaps/overlap_ver5/inspect_whole/output/human_d20/extended
+#
